fc08c0176e35bcc66cd74028a636e3a8714780fb
Nilufar <nilufardev@gmail.com>
2022-09-18 19:52:17 +0500

    Initial Commit

---
+++ Test3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by Nilufar Ulug’bek on 18/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
